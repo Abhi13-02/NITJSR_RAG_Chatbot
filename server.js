1cb05706ebb0b6f9ec452d1474a39acfb85dc9e7
Pritam Roy <ratsdust4226@gmail.com>
2025-11-08 17:05:52 +0530

    rate limiting 1

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { MongoClient } from 'mongodb';
+import { createClient as createRedisClient } from 'redis';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -54,6 +56,10 @@ class NITJSRServer {
       delay: 1500,
     });
     this.isInitialized = false;
+
+    // NEW: place to store redis client
+    this.redis = null;
+
     this.setupMiddleware();
     this.setupRoutes();
   }
